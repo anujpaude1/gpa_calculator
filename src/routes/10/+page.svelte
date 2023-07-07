80ae1577cd87d953522e08f31fd52994aca1e099
Prateek61 <prateekpoudel61@gmail.com>
2023-07-07 23:44:58 +0545

    implemented front page

---
+++ src/routes/10/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    
+</script>
+
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-10 text-center flex flex-col items-center">
+		<h1 class="text-6xl font-bold">GPA Calculator</h1>
+
+		<h1 class="text-2xl font-bold">Choose your field</h1>
+	</div>
+</div>
